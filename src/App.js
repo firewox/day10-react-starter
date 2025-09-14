1df7582b2c6045a1a03b994e6cbcc9723a888628
LIJA30 <jade.li@oocl.com>
2025-09-14 14:50:43 +0800

    refactor: adjust page style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useReducer } from "react";
 import "./App.css"
-import {TodoContext, initState} from "./contexts/TodoContext"
+import { TodoContext, initState } from "./contexts/TodoContext"
 import todoReducer from "./reducers/TodoReducer"
 import TodoGroup from "./components/TodoGroup"
 import TodoAdd from "./components/TodoAdd"
@@ -8,10 +8,15 @@ import TodoAdd from "./components/TodoAdd"
 function App() {
   const [state, dispatch] = useReducer(todoReducer, initState);
   return (
-    <div>
+    <div className="App">
       <TodoContext.Provider value={{ state, dispatch }}>
-        <TodoGroup />
-        <TodoAdd />
+        <header>
+          <h1>Todo List</h1>
+        </header>
+        <context>
+          <TodoGroup />
+          <TodoAdd />
+        </context>
       </TodoContext.Provider>
     </div>
   );
